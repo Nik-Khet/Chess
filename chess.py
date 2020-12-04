bf8a7e2c14c98a311783e78b2af82c5e6434b5ab
Nik-Khet <67691373+Nik-Khet@users.noreply.github.com>
2020-12-04 21:12:14 +0000

    Initial setup
    Board class and piece class setup. Started development of Queen subclass, making an initial attempt at identifying valid moves for the piece.

---
+++ chess.py
@@ -0,0 +1,60 @@
+import numpy as np
+class board(object):
+    def __init__(self):
+        self.state = np.zeros(8,8)
+        pass
+    
+    
+    def printboard(self):
+        print(self.state)
+        pass
+    def updateboard(self, piece):
+        self.state[piece.row,piece.col]=piece
+
+
+
+class piece(object): 
+    def __init__(self, row, col, colour, board):
+        self.board = board
+        self.row = row
+        self.col = col
+        self.colour = colour
+        self.moves = []
+        self.king = False
+        self.board
+        pass
+    
+    def __repr__(self):
+        return 
+    def getpos(self):
+        return [self.col,self.row]
+    
+    
+class Queen(piece):
+    def __init__(self):
+        self.name = "Queen"
+        pass
+    def moves(self):
+        x = self.col
+        y = self.row
+        chessboard = self.board.state
+        moves = []
+        #Straight up
+        valid=True
+        count=0
+        while valid:
+            if count!=0:
+                moves.append((x,y-count))
+            count+=1
+            if x+1<0:
+                valid=False
+            if chessboard[y][x]!=0:
+                target_piece = chessboard[y][x]
+                if target_piece.colour == self.colour:
+                    valid=False
+        return moves
+
+
+chessboard = board()
+
+chessboard.printboard()
